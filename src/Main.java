97e1f4fe2e3986badc6d4d5f4e40513aa74ff35c
2br-2b <johnwuller@nildas-air-2.attlocal.net>
2019-04-09 21:13:57 -0500

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+public class Main {
+
+	public static void main(String args[]) throws Exception{
+		System.out.println("Hello there.");
+		JDA jda = new JDABuilder("NTY1MDE4NDMzMjAxMzczMjE1.XK1Qzg.QnN97RMGr0XayGrnQkXmaYQ2X6U").build();
+		
+		//jda.addEventListener(listeners);
+		
+			
+	}
+
+}
