2afdb3e542d85af7eedcc973005758f6f22c8c76
GreedyVagabond <49485327+GreedyVagabond@users.noreply.github.com>
2019-04-10 09:24:32 -0400

    See if that works

--- src/src/Main.java
+++ src/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String args[]) throws Exception{
 		System.out.println("Java vs Python");
-		JDA jda = new JDABuilder("NTY1MDE4NDMzMjAxMzczMjE1.XK1Qzg.QnN97RMGr0XayGrnQkXmaYQ2X6U").build();
+		JDA jda = new JDABuilder(AccountType.BOT).setToken("NTY1MDE4NDMzMjAxMzczMjE1.XK1Qzg.QnN97RMGr0XayGrnQkXmaYQ2X6U").build();
 		
 		jda.addEventListener(new Test());
 		
